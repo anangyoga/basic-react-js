f99b92ded9058bb2788b213e0573eabaddef03f1
anangyoga <aharwinto@gmail.com>
2021-09-15 05:23:20 +0700

    starting React, statefull & stateless component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloComponent from './components/HelloComponent';
+import StateFullComponentn from './container/StateFullComponent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloComponent />
+    <StateFullComponentn />
   </React.StrictMode>,
   document.getElementById('root')
 );
